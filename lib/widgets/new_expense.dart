ec5f8818cc77927a1f6ab048727bc9f834a403ad
Cecily Toro <torocecily@gmail.com>
2024-05-15 12:21:18 -0400

    add text inputs to modal overlay

--- lib/widgets/new_expense.dart
+++ lib/widgets/new_expense.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class NewExpense extends StatefulWidget {
   const NewExpense({super.key});
@@ -9,17 +11,75 @@ class NewExpense extends StatefulWidget {
 }
 
 class _NewExpenseState extends State<NewExpense> {
+  final _titleController = TextEditingController();
+  final _amountContoller = TextEditingController();
+  //whenever using a TextEditingController, make sure to tell Flutter to delete it
+  // when the widget is not in use. (when the modal overlay closes.):
+  @override
+  void dispose() {
+    _titleController.dispose();
+    _amountContoller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Padding(
-      padding: EdgeInsets.all(16),
+    return Padding(
+      padding: const EdgeInsets.all(16),
       child: Column(
         children: [
           TextField(
+            controller: _titleController,
             maxLength: 50,
-            decoration: InputDecoration(label: Text('Title')),
-            keyboardType: TextInputType.text,
+            decoration: const InputDecoration(
+              label: Text('Title'),
+            ),
           ),
+          Row(
+            children: [
+              Expanded(
+                child: TextField(
+                  controller: _amountContoller,
+                  decoration: const InputDecoration(
+                    prefixText: '\$ ',
+                    label: Text('Amount'),
+                  ),
+                  keyboardType: TextInputType.number,
+                ),
+              ),
+              const SizedBox(width: 16),
+              Expanded(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    const Text('Selected Date'),
+                    IconButton(
+                      onPressed: () {},
+                      icon: const Icon(Icons.calendar_month),
+                    )
+                  ],
+                ),
+              ),
+            ],
+          ),
+          Row(
+            children: [
+              ElevatedButton(
+                onPressed: () {
+                  print(_titleController.text);
+                  print(_amountContoller.text);
+                },
+                child: const Text('Save Expense'),
+              ),
+              TextButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: const Text('Cancel'),
+              )
+            ],
+          )
         ],
       ),
     );
